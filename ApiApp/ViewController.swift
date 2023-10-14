3d5509fb5cd9fa21707183391191375f7017995a
taro.kirameki <taro.kirameki@techacademy.jp>
2023-10-14 10:52:26 +0900

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by taro.kirameki on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
